8ef7d387b0ed0dadb2829c08f53509e77d81bed8
LOORD.Y! <151580584+LOORDyassin@users.noreply.github.com>
2024-03-23 01:14:03 +0100

    L

--- users.php
+++ users.php
@@ -1,21 +1,26 @@
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>my chat</title>
-<link rel="stylesheet" href="style.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
-</head>
+<?php 
+    session_start();
+    if(!isset($_SESSION['unique_id'])){
+        header("location: login.php");
+    }
+?>
+<?php include_once "header.php"; ?>
 <body>
     <div class="wrapper">
         <section class="users">
             <header>
+            <?php 
+                include_once "php/config.php";
+                $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+                if(mysqli_num_rows($sql) > 0){
+                $row = mysqli_fetch_assoc($sql);
+                }
+            ?>    
                 <div class="content">
-                    <img src="LOORD.png" alt="">
+                    <img src="php/images/<?php echo $row["img"] ?>" alt="">
                     <div class="details">
-                        <span>Coding Nepal</span>
-                        <p>Active now</p>
+                        <span><?php echo $row["fname"] . " " . $row["lname"] ?></span>
+                        <p><?php echo $row["status"] ?></p>
                     </div>
                 </div>
                 <a href="#" class="logout">Logout</a>
@@ -25,77 +30,7 @@
                 <input type="text" placeholder="Enter name to search...">
                 <button><i class="fas fa-search"></i></button>
             </div>
-            <div class="users-list">
-                <a href="#">
-                <div class="content">
-                    <img src="LOORD.png" alt="">
-                    <div class="details">
-                        <span>Coding Nepal</span>
-                        <p>This is test message</p>
-                    </div>
-                </div>
-                <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="LOORD.png" alt="">
-                        <div class="details">
-                            <span>Coding Nepal</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="LOORD.png" alt="">
-                        <div class="details">
-                            <span>Coding Nepal</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="LOORD.png" alt="">
-                        <div class="details">
-                            <span>Coding Nepal</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="LOORD.png" alt="">
-                        <div class="details">
-                            <span>Coding Nepal</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="LOORD.png" alt="">
-                        <div class="details">
-                            <span>Coding Nepal</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="LOORD.png" alt="">
-                        <div class="details">
-                            <span>Coding Nepal</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
+            <div class="users-list">  
             </div>
         </section>
     </div>
