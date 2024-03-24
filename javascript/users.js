6cf7a65aa5eb41d83755798d5e0db4b279817f27
LOORD.Y! <151580584+LOORDyassin@users.noreply.github.com>
2024-03-24 23:03:25 +0100

    100

--- javascript/users.js
+++ javascript/users.js
@@ -1,11 +1,14 @@
-const searchBar = document.querySelector(".users .search input");
+const searchBar = document.querySelector(".users .search input"),
 searchBtn = document.querySelector(".users .search button"),
 usersList = document.querySelector(".users .users-list");
+
 searchBtn.onclick = ()=>{
     searchBar.classList.toggle("active");
     searchBar.focus();
     searchBtn.classList.toggle("active");
 }
+
+
 
 setInterval(()=>{
     // let's start Ajax
