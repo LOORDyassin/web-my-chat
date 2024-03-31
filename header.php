18294573af1e0bd17b20b36152c4d709eb2b8fa9
LOORD.Y! <151580584+LOORDyassin@users.noreply.github.com>
2024-03-31 21:39:37 +0200

    icon&name

--- header.php
+++ header.php
@@ -3,8 +3,8 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>my chat</title>
+<title>chat-box</title>
 <link rel="stylesheet" href="style.css">
-<link href="img/favicon.ico" rel="shortcut icon"/>
+<link href="messenger.png" rel="shortcut icon"/>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
 </head>
